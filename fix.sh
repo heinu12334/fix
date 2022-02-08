c9a34758569d8c21317ab12617d5d4b1e98467a7
heinu12334 <90031446+heinu12334@users.noreply.github.com>
2022-02-09 03:53:10 +0800

    Update fix.sh

--- fix.sh
+++ fix.sh
@@ -1,8 +1,7 @@
   if [ `id -u` != 0 ];then
     echo " (该功能仅限 root 用户执行)"
  else
- echo "开始清空防火墙规则/停止防火墙/卸载防火墙..."
-  if check_sys packageManager yum; then
+ echo "开始修复..."
     systemctl stop firewalld.service
     systemctl disable firewalld.service
     systemctl stop iptables
@@ -10,12 +9,10 @@
     service stop iptables
     yum remove -y iptables
     yum remove -y firewalld
-  elif check_sys packageManager apt; then
     iptables -F
     service stop iptables
     apt-get remove -y iptables
     ufw disable
-  fi 
   echo "修复完成"
   echo "如果代理还是无法链接请使用 lsof -i:端口号 命令查询端口是否被占用"
   fi
